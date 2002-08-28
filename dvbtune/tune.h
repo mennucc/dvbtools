a6aff6525bd034f29c18c8510e9d5b1ed9f96826
Dave Chapman <linuxstb@users.sourceforge.net>
2002-08-28 23:06:58 +0000

    more NEWSTRUCT patches

--- dvbtune/tune.h
+++ dvbtune/tune.h
@@ -1,7 +1,11 @@
 #ifndef _TUNE_H
 #define _TUNE_H
 
-#include <ost/frontend.h>
+#ifdef NEWSTRUCT
+  #include <linux/dvb/frontend.h>
+#else
+  #include <ost/frontend.h>
+#endif
 #include "dvb_defaults.h"
 
 int tune_it(int fd_frontend, int fd_sec, unsigned int freq, unsigned int srate, char pol, int tone, SpectralInversion specInv, unsigned int diseqc,Modulation modulation,CodeRate HP_CodeRate,TransmitMode TransmissionMode,GuardInterval guardInterval, BandWidth bandwidth);
