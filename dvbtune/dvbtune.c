66e37580ec36104fb99dc9509a83617bde9f9757
Nico <nicosb@users.sourceforge.net>
2005-01-06 10:36:28 +0000

    added support for ATSC

--- dvbtune/dvbtune.c
+++ dvbtune/dvbtune.c
@@ -705,7 +705,10 @@ int main(int argc, char **argv)
     fprintf(stderr,"-c [0-3]    Use DVB card #[0-3]\n");
     fprintf(stderr,"-I [0|1|2]  0=Spectrum Inversion off, 1=Spectrum Inversion on, 2=auto\n");
     fprintf(stderr,"-D [1-4AB]    DiSEqC command\n\n");
-    fprintf(stderr,"-qam X      DVB-T modulation - 16%s, 32%s, 64%s, 128%s or 256%s\n",(CONSTELLATION_DEFAULT==QAM_16 ? " (default)" : ""),(CONSTELLATION_DEFAULT==QAM_32 ? " (default)" : ""),(CONSTELLATION_DEFAULT==QAM_64 ? " (default)" : ""),(CONSTELLATION_DEFAULT==QAM_128 ? " (default)" : ""),(CONSTELLATION_DEFAULT==QAM_256 ? " (default)" : ""));
+    fprintf(stderr,"-qam X      DVB-T/C and ATSC modulation - 16%s, 32%s, 64%s, 128%s or 256%s\n",(CONSTELLATION_DEFAULT==QAM_16 ? " (default)" : ""),(CONSTELLATION_DEFAULT==QAM_32 ? " (default)" : ""),(CONSTELLATION_DEFAULT==QAM_64 ? " (default)" : ""),(CONSTELLATION_DEFAULT==QAM_128 ? " (default)" : ""),(CONSTELLATION_DEFAULT==QAM_256 ? " (default)" : ""));
+#ifdef DVB_ATSC
+    fprintf(stderr,"-vsb X      ATSC modulation - 8, 16\n");
+#endif
     fprintf(stderr,"-gi N       DVB-T guard interval 1_N (N=32%s, 16%s, 8%s or 4%s)\n",(GUARD_INTERVAL_DEFAULT==GUARD_INTERVAL_1_32 ? " (default)" : ""),(GUARD_INTERVAL_DEFAULT==GUARD_INTERVAL_1_16 ? " (default)" : ""),(GUARD_INTERVAL_DEFAULT==GUARD_INTERVAL_1_8 ? " (default)" : ""),(GUARD_INTERVAL_DEFAULT==GUARD_INTERVAL_1_4 ? " (default)" : ""));
     fprintf(stderr,"-cr N       DVB-T code rate. N=AUTO%s, 1_2%s, 2_3%s, 3_4%s, 5_6%s, 7_8%s\n",(HP_CODERATE_DEFAULT==FEC_AUTO ? " (default)" : ""),(HP_CODERATE_DEFAULT==FEC_1_2 ? " (default)" : ""),(HP_CODERATE_DEFAULT==FEC_2_3 ? " (default)" : ""),(HP_CODERATE_DEFAULT==FEC_3_4 ? " (default)" : ""),(HP_CODERATE_DEFAULT==FEC_5_6 ? " (default)" : ""),(HP_CODERATE_DEFAULT==FEC_7_8 ? " (default)" : ""));
     fprintf(stderr,"-bw N       DVB-T bandwidth (Mhz) - N=6%s, 7%s or 8%s\n",(BANDWIDTH_DEFAULT==BANDWIDTH_6_MHZ ? " (default)" : ""),(BANDWIDTH_DEFAULT==BANDWIDTH_7_MHZ ? " (default)" : ""),(BANDWIDTH_DEFAULT==BANDWIDTH_8_MHZ ? " (default)" : ""));
@@ -779,7 +782,20 @@ int main(int argc, char **argv)
             fprintf(stderr,"Invalid QAM rate: %s\n",argv[i]);
             exit(0);
         }
-      } else if (strcmp(argv[i],"-gi")==0) {
+      } 
+#ifdef DVB_ATSC
+     else if(strcmp(argv[i],"-vsb")==0) {
+       i++;
+        switch(atoi(argv[i])) {
+	  case 8:  modulation=VSB_8; break;
+          case 16:  modulation=VSB_16; break;
+          default:
+            fprintf(stderr,"Invalid ATSC VSB modulation: %s\n",argv[i]);
+            exit(0);
+        }
+      }
+#endif
+      else if (strcmp(argv[i],"-gi")==0) {
         i++;
         switch(atoi(argv[i])) {
           case 32:  guardInterval=GUARD_INTERVAL_1_32; break;
